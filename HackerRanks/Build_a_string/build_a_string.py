b616525b0647207ecc84b2de176e8219b90bf3ae
Oscar <oscar.atncp@territoriumlife.com>
2019-11-25 16:07:54 -0600

    change in find_substring

--- HackerRanks/Build_a_string/build_a_string.py
+++ HackerRanks/Build_a_string/build_a_string.py
@@ -22,19 +22,19 @@ def get_substrings(s,a,b):
 
 def find_substring(s, sub):
     s_list= []
-    s_list.append(s[1:])
+    s_list.append(s)
     tail = set(s_list)
     if len(tail &sub) > 0 :
         return tail & sub
     else:
-        return find_substring(s[1:],sub)
+        return find_substring(s[:-1],sub)
 
 
 def buildString(a, b, s):
     res = 0
     
     substrings = get_substrings(s,a,b)
-    res = find_substring(s,substrings)
+    res = find_substring(s[1:],substrings)
     return res
 
  
