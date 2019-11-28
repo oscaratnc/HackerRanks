1ad47779d265fde4b834bc54b0ed0b2fd54d3d10
Oscar <oscar.atncp@territoriumlife.com>
2019-11-27 19:40:18 -0600

    Migrations and new models

--- Doggify/doggify/Dogs/models.py
+++ Doggify/doggify/Dogs/models.py
@@ -5,7 +5,7 @@ class Dog(models.Model):
     name  = models.TextField(max_length=50) 
     age =  models.IntegerField()
     gender = models.BooleanField()
-    size =  models.DecimalField(decimal_places=2)
+    size =  models.DecimalField(decimal_places=2, max_digits=2)
     photo = models.TextField(max_length=50)
     breed =  models.CharField(max_length = 3, choices = [
         ("BTR", 'Boston Terrier'),
@@ -13,17 +13,24 @@ class Dog(models.Model):
         ("CHW", 'ChowChow'),
         ("PUG", 'Pug'),
     ])
+class Shelter(models.Model):
+    name  =  models.TextField(max_length= 20)
+    owner = models.ForeignKey(User, on_delete= models.DO_NOTHING)
+    address = models.TextField(max_length= 60)
+    phone = models.TextField(max_length=10)
 
 class Adoption(models.Model):
     dog = models.ForeignKey(Dog, on_delete=models.DO_NOTHING)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='user')
     shelter = models.ForeignKey(Shelter, on_delete=models.DO_NOTHING)
     adoption_date = models.DateTimeField(auto_now= True)
 
-class Shelter(models.Model):
-    name  =  models.TextField(max_length= 20)
-    owner = models.TextField(max_length= 30)
-    address = models.TextField(max_length= 60)
-    phone = models.TextField(max_length=10)
+class AdoptionRequest(models.Model):
+    dog = models.ForeignKey(Dog, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    shelter_owner = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='shelter_owner')
+    adoption_date = models.DateTimeField(auto_now= True)
+
+
 
   
